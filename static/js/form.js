5bffb104b732cfa6f9c226adb56915e73aaad5ea
larrysoncruz <larrycruz677@gmail.com>
2024-10-10 19:29:41 -0500

    version1

---
+++ static/js/form.js
@@ -0,0 +1,11 @@
+function registrar_user(){
+    let nombre = document.getElementById("name").value
+    let apellido = document.getElementById("lastname").value
+    let cedula = document.getElementById("id").value
+    let telefono = document.getElementById("phone").value
+    let fecha = document.getElementById("date").value
+    let email = document.getElementById("email").value
+    let contrasena = document.getElementById("passw").value
+
+    document.write(nombre + " " + apellido + " " + cedula + " " + telefono + " " +  fecha + " " + email + " " + contrasena )
+}
